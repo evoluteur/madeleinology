8493e54d2d050e8c66e3c2017cd99dd36f8586a1
Olivier Giulieri <ogiulieri@gmail.com>
2024-04-26 04:48:08 -0700

    Text edits + cleanups.

--- js/recipes.js
+++ js/recipes.js
@@ -1,68 +1,81 @@
+const elems = {};
+const $ = (id) => {
+  if (!elems[id]) {
+    elems[id] = document.getElementById(id);
+  }
+  return elems[id];
+};
 
-function list(){
-	return recipes.map(function(r){
-		return '<div>'+
-					//'<img src="'+r['image-src']+'">'+
-					'<div class="ing-chart">'+
-						bar(r, 'farine')+bar(r, 'sucre')+bar(r, 'beurre')+bar(r, 'oeuf')+
-					'</div>'+
-					//'<div>'+r.title+'</div>'+
-					'<div><a target="_blank" href="'+r.url+'" class="r-title">'+r.title+'</a></div>'+
-					'<div class="votes">'+
-						(r.votes?(stars(r.score)+' ('+r.votes+' votes)'):'(0 votes)')+
-					'</div>'+
-					'<div class="xtra">'+
-						r.description+
-					'</div>'+
-				'</div>';
-	}).join('');
-}
-function stars(n){
-	return Array(n+1).join('★')
-}
-var usNames={
-	'farine': 'flour',
-	'sucre': 'sugar',
-	'beurre': 'butter',
-	'oeuf': 'egg',
-}
-function bar(r, name){
-	var n=Math.round(r['pc_'+name]);
-	return '<div class="bar" style="height:'+n+'px;"><div>'+n+'% '+usNames[name]+'</div></div>'
-}
+const list = () =>
+  recipes
+    ?.map(
+      (r) =>
+        "<div>" +
+        //'<img src="'+r['image-src']+'">'+
+        '<div class="ing-chart">' +
+        bar(r, "farine") +
+        bar(r, "sucre") +
+        bar(r, "beurre") +
+        bar(r, "oeuf") +
+        "</div>" +
+        //'<div>'+r.title+'</div>'+
+        `<div><a target="_blank" href="${r.url}" class="r-title">${r.title}</a></div>` +
+        '<div class="votes">' +
+        (r.votes ? stars(r.score) + " (" + r.votes + " votes)" : "(0 votes)") +
+        "</div>" +
+        `<div class="xtra">${r.description}</div>` +
+        "</div>"
+    )
+    .join("");
 
-function render(){
-	document.getElementById('m-list').innerHTML = list();
-}
+const stars = (n) => Array(n + 1).join("★");
 
-var prevSort='';
-function sort(n){
-	var fnSort;
+var usNames = {
+  farine: "flour",
+  sucre: "sugar",
+  beurre: "butter",
+  oeuf: "egg",
+};
 
-	if(n==='title'){
-		fnSort = function(a, b){
-			return a.title.localeCompare(b.title);
-		}
-	}else if(n==='score'){
-		fnSort = function(a, b){
-			return (1000*b[n]+b.votes) - (1000*a[n]+a.votes);
-		}
-	}else{
-		fnSort = function(a, b){
-			return b[n] - a[n];
-		}
-	}
-
-	if(prevSort==n){
-		recipes.sort(function(a,b){return -1*fnSort(a,b)});
-		prevSort=n+'-asc';
-	}else{
-		recipes.sort(fnSort);
-		prevSort=n;
-	}
-	render();
+function bar(r, name) {
+  const n = Math.round(r["pc_" + name]);
+  return `<div class="bar" style="height:${n + "px"}"><div> ${
+    n + "% " + usNames[name]
+  }
+  </div></div>`;
 }
 
-document.addEventListener("DOMContentLoaded", function(event) { 
+function render() {
+  $("m-list").innerHTML = list();
+}
+
+var prevSort = "";
+function sort(n) {
+  var fnSort;
+  if (n === "title") {
+    fnSort = function (a, b) {
+      return a.title.localeCompare(b.title);
+    };
+  } else if (n === "score") {
+    fnSort = function (a, b) {
+      return 1000 * b[n] + b.votes - (1000 * a[n] + a.votes);
+    };
+  } else {
+    fnSort = function (a, b) {
+      return b[n] - a[n];
+    };
+  }
+
+  if (prevSort == n) {
+    recipes.sort(function (a, b) {
+      return -1 * fnSort(a, b);
+    });
+    prevSort = n + "-asc";
+  } else {
+    recipes.sort(fnSort);
+    prevSort = n;
+  }
   render();
-});
+}
+
+window.onload = render;
